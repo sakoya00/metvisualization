69b8e7a658ba48cbb56a804ad1728e10473d0291
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-07-26 02:15:06 -0500

    Add files via upload

---
+++ artistcount.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+#setwd(D:/Met Object)
+#getwd()
+
+#calculates artists with most Met artworks in descending order
+met <- read.csv("metobjects.csv", header= TRUE, na.strings=c("", "NA"))
+artname <- met[,17]
+artname <- na.omit(artname)
+artistcount <- count(artname, sort = TRUE)
+
+
